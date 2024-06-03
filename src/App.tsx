dd9b3665677b417a5756935b1d93a2b7a2ddd251
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-03 09:45:57 +0200

    refactored useMetamask out of app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,22 @@
 import "./App.css";
-import { useState, useEffect } from "react";
-//import { ethers } from 'ethers';
+import { useMetaMask } from "./useMetaMask";
 
 function App() {
-  const [walletAddress, setWalletAddress] = useState("");
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Handle mobile environment
-  useEffect(() => {
-    // Check if the user is on a mobile device
-    const userAgent = navigator.userAgent;
-    const isMobileDevice = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent.toLowerCase());
-    setIsMobile(isMobileDevice);
-  }, []);
-
-  async function requestAccount() {
-    // Check if Metamask exists
-    if (window.ethereum) {
-      console.log("Metamask detected");
-      try {
-        const accounts = await window.ethereum.request({
-          method: "eth_requestAccounts",
-        });
-        setWalletAddress(accounts[0]);
-      } catch (error) {
-        console.log("Error connecting...");
-      }
-    } else {
-      console.log("Metamask not detected");
-    }
-  }
-
-  useEffect(() => {
-    if (window.ethereum) {
-      // Listen fo account changes
-      const handleAccountsChanged = (accounts: string[]) => {
-        setWalletAddress(accounts[0]);
-      };
-
-      window.ethereum.on("accountsChanged", handleAccountsChanged);
-
-      // Cleanup function to remove the listener when then component unmounts
-      return () => {
-        window.ethereum.removeListener("accountsChanged", handleAccountsChanged);
-      };
-    }
-  }, []); // Empty dependency array ensures this runs once when the component mounts
+  
+  const { walletAddress, isMobile, requestAccount } = useMetaMask();
 
   return (
     <main>
       <div>
         <button onClick={requestAccount}>Connect MetaMask</button>
         {isMobile && (
-          <a href="http://metamask.app.link/dapp/some-air-hissing.on-fleek.app/" target="_blank" rel="noopener noreferrer">Open MetaMask</a>
+          <a
+            href="http://metamask.app.link/dapp/some-air-hissing.on-fleek.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open MetaMask
+          </a>
         )}
         <h2>Wallet Address: {walletAddress} </h2>
       </div>
---
+++ src/useMetaMask.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+export function useMetaMask() {
+  const [walletAddress, setWalletAddress] = useState<string>("");
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  // Handle mobile environment
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const isMobileDevice =
+      /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+        userAgent.toLowerCase()
+      );
+    setIsMobile(isMobileDevice);
+  }, []);
+
+  // Get an account from MetaMask
+  async function requestAccount() {
+    if (window.ethereum) {
+      console.log("Metamask detected");
+      try {
+        const accounts: string[] = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log("Error connecting...");
+      }
+    } else {
+      console.log("Metamask not detected");
+    }
+  }
+
+  // Listen for account changes and reflect them in front-end
+  useEffect(() => {
+    if (window.ethereum) {
+      const handleAccountsChanged = (accounts: string[]) => {
+        setWalletAddress(accounts[0]);
+      };
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+      // Cleanup function to remove the listener when then component unmounts
+      return () => {
+        window.ethereum.removeListener(
+          "accountsChanged",
+          handleAccountsChanged
+        );
+      };
+    }
+  }, []); // Empty dependency array ensures this runs once when the component mounts
+
+  return { walletAddress, isMobile, requestAccount };
+}
