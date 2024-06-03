6050027e7370f169e22ab8ccdb57996b1db4035e
Wim Stalmans <wim.stalmans@gmail.com>
2024-06-03 08:00:44 +0200

    handleAccountsChanged

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import "./App.css";
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 //import { ethers } from 'ethers';
 
 function App() {
-
   const [walletAddress, setWalletAddress] = useState("");
 
   async function requestAccount() {
     // Check if Metamask exists
     if (window.ethereum) {
       console.log("Metamask detected");
-
       try {
         const accounts = await window.ethereum.request({
           method: "eth_requestAccounts",
@@ -23,6 +21,22 @@ function App() {
       console.log("Metamask not detected");
     }
   }
+
+  useEffect(() => {
+    if (window.ethereum) {
+      // Listen fo account changes
+      const handleAccountsChanged = (accounts: string[]) => {
+        setWalletAddress(accounts[0]);
+      };
+
+      window.ethereum.on("accountsChanged", handleAccountsChanged);
+
+      // Cleanup function to remove the listener when then component unmounts
+      return () => {
+        window.ethereum.removeListener("accountsChanged", handleAccountsChanged);
+      };
+    }
+  }, []); // Empty dependency array ensures this runs once when the component mounts
 
   return (
     <main>
